38736525729bfef0d167593084dee7cdb499d61c
Egorov Anatolii <silkroadbum@mail.ru>
2024-07-03 18:44:55 +0300

    configuration routing

---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,17 @@
+import {Route, Routes} from "react-router-dom";
+import {Suspense} from "react";
+import {routeConfig} from "shared/config/routerConfig/routerConfig";
+
+const AppRouter = () => {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+                {Object.values(routeConfig).map(({element, path}) => (
+                    <Route key={path} path={path} element={element}/>
+                ))}
+            </Routes>
+        </Suspense>
+    )
+};
+
+export default AppRouter;
