6c37b931d9e3221b51cd97f6255e7877f1bb2335
Egorov Anatolii <silkroadbum@mail.ru>
2024-07-07 17:57:52 +0300

    add theme icons and add Button component

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,7 @@
 import {classNames} from "shared/lib/classNames/classNames";
 import {FC} from "react";
 import cls from './Navbar.module.scss';
+import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
 import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
 
 interface NavbarProps {
@@ -9,6 +10,7 @@ interface NavbarProps {
 export const Navbar: FC<NavbarProps> = ({className}) => {
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
+            <ThemeSwitcher/>
             <div className={cls.links}>
                 <AppLink theme={AppLinkTheme.SECONDARY} to={"/"} className={cls.mainLink}>
                     Главная
