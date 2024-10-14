a0ecde37323fbc3e058fc1c63162fc2cf497fbac
Egorov Anatolii <silkroadbum@mail.ru>
2024-10-15 00:09:33 +0300

    add storybook and stories for Button

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,20 +1,23 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { FC } from 'react';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
     className?: string
 }
-export const Navbar: FC<NavbarProps> = ({ className }) => (
-  <div className={classNames(cls.Navbar, {}, [className])}>
-    <div className={cls.links}>
-      <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>
-        Главная
-      </AppLink>
-      <AppLink theme={AppLinkTheme.SECONDARY} to="/about">
-        О сайте
-      </AppLink>
+export const Navbar: FC<NavbarProps> = ({ className }) => {
+  const { t } = useTranslation(); return (
+    <div className={classNames(cls.Navbar, {}, [className])}>
+      <div className={cls.links}>
+        <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>
+          {t('Главная')}
+        </AppLink>
+        <AppLink theme={AppLinkTheme.SECONDARY} to="/about">
+          {t('О сайте')}
+        </AppLink>
+      </div>
     </div>
-  </div>
-);
+  );
+};
