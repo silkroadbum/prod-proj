7c098582bd4afffca42368b2e37411b30534f22a
Egorov Anatolii <silkroadbum@mail.ru>
2024-10-14 21:33:15 +0300

    add Button test

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -4,16 +4,6 @@
  */
 
 export default {
-  // All imported modules in your tests should be mocked automatically
-  // automock: false,
-
-  // Stop running tests after `n` failures
-  // bail: 0,
-
-  // The directory where Jest should store its cached dependency information
-  // cacheDirectory: "C:\\Users\\Anatolii\\AppData\\Local\\Temp\\jest",
-
-  // Automatically clear mock calls, instances and results before every test
   clearMocks: true,
   testEnvironment: 'jsdom',
   coveragePathIgnorePatterns: [
@@ -30,10 +20,17 @@ export default {
   moduleDirectories: [
     'node_modules',
   ],
+  modulePaths: [
+    '<rootDir>src',
+  ],
   testMatch: [
     '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
   ],
   rootDir: '../../',
+  setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
+  moduleNameMapper: {
+    '\\.(s?css)$': 'identity-obj-proxy',
+  },
 
   // Indicates whether the coverage information should be collected while executing the test
   // collectCoverage: false,
